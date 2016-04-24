9004d54d1ac49989d2fd09e447fb14c46554ce3d
bdkpr <bdkpr@missionrnd.com>
2016-04-24 12:29:45 +0530

    Submit Commit

--- src/MainMrndTest3.cpp
+++ src/MainMrndTest3.cpp
@@ -42,51 +42,6 @@ void printBST(struct node * root){
 	}
 }
 
-struct enode *newENode_spec(char *str)
-{
-	struct enode *temp = (struct enode *)malloc(sizeof(struct enode));
-	int i = 0;
-	while (str[i] != '\0'){
-		temp->data[i] = str[i];
-		i++;
-	}
-	temp->data[i] = '\0';
-	temp->left = NULL;
-	temp->right = NULL;
-	return temp;
-}
-struct enode * constructEBinaryTree_spec(char strs[][6], int len){
-	struct enode *root = NULL;
-	struct enode *nodes[2005];
-	for (int i = 0; i < len; i++){
-		nodes[i] = newENode_spec(strs[i]);
-	}
-	int mid = (len / 2);
-	for (int i = 0; i < mid; i++){
-		struct enode *curnode = nodes[i];
-		struct enode *leftnode = nodes[(i * 2) + 1];
-		struct enode *rightnode = nodes[(i * 2) + 2];
-		if (curnode->data[0] != 'X'){
-			if (leftnode->data[0] != 'X'){
-				curnode->left = leftnode;
-			}
-			if (rightnode->data[0] != 'X'){
-				curnode->right = rightnode;
-			}
-		}
-	}
-	root = nodes[0];
-	return root;
-}
-void test_Problem3_spec(char strs[][6], int len, int ans){
-	struct enode *root = constructEBinaryTree_spec(strs, len);
-	int actual = solve_tree(root);
-	//Assert::AreEqual(ans, actual, L"Failed sample 0 for NULL case in P1", 1, 2);
-}
 int main(){
 	//Test here
-	char strs[][6] = { "+", "5", "*", "X", "X", "a", "3" };
-	int len = 7;
-	int ans = -1;
-	test_Problem3_spec(strs, len, ans);
 }
